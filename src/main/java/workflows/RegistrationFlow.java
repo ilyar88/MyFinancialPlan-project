69c22459ef1846c583227b147cb594470f6995b9
ilyar88 <irahmilevich@gmail.com>
2025-09-30 14:08:27 +0300

    Update RegistrationFlow class.

--- src/main/java/workflows/RegistrationFlow.java
+++ src/main/java/workflows/RegistrationFlow.java
@@ -1,14 +1,17 @@
 package workflows;
 
-import extensions.UiActions;
+import extensions.*;
 import utilities.ManagePages;
 
+
 public class RegistrationFlow {
+	
 	public static void userRegister(String email, String password) {
 		UiActions.click(ManagePages.login().createAccountLink());
 		UiActions.enterText(ManagePages.register().emailAddress(), email);
 		UiActions.enterText(ManagePages.register().password(), password);
-		UiActions.enterText(ManagePages.register().passwordVerification(), password);
+		UiActions.enterText(ManagePages.register().passwordConfirmation(), password);
 		UiActions.click(ManagePages.register().CreateAccount());
+		Verifications.isDisplayed(ManagePages.otp().registerImage(),true);
 	}
 }
