2f204f4b41f1e54e2b45bbdd02df9298d96cb67c
ilyar88 <irahmilevich@gmail.com>
2025-09-30 23:57:47 +0300

    Add ProfilePage class.

---
+++ src/main/java/pageObjects/ProfilePage.java
@@ -0,0 +1,39 @@
+package pageObjects;
+
+import java.util.List;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class ProfilePage {
+	private  WebDriver _driver;
+	
+	public ProfilePage(WebDriver driver) {
+		_driver = driver;
+    }
+	
+    public WebElement header() {
+        return _driver.findElement(By.cssSelector("h1.MuiTypography-root.MuiTypography-h1.css-h3u9dv-MuiTypography-root"));
+    }
+    
+    public WebElement firstName() {
+        return _driver.findElement(By.cssSelector("input[name='firstName']"));
+    }
+    
+    public WebElement age() {
+        return _driver.findElement(By.cssSelector("input[name='age']"));
+    }
+    
+    public WebElement lastName() {
+        return _driver.findElement(By.cssSelector("input[name='lastName']"));
+    }
+    
+    public List<WebElement> dropdowns() {
+    	return _driver.findElements(By.cssSelector("select.MuiNativeSelect-select"));
+    }
+    
+    public WebElement createProfile() {
+        return _driver.findElement(By.cssSelector("button[type='submit']"));
+    }
+}
