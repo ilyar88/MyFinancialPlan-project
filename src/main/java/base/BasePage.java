d0125afa1c5177fbd90b0bd5e0120414bbf3fdcf
Ilya Rahmilevich <110979314+ilyar88@users.noreply.github.com>
2025-10-01 21:43:02 +0300

    Update BasePage.java file.

--- src/main/java/base/BasePage.java
+++ src/main/java/base/BasePage.java
@@ -61,7 +61,7 @@ public class BasePage {
         driver.manage().window().setSize(new Dimension(1920, 1080));
         int waitSeconds = Integer.parseInt(prop.getProperty("waitTime"));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(waitSeconds));
-        driver.get(System.getenv("URL"));
+        driver.get(System.getProperty("URL", System.getenv("URL")));
         ManagePages.init(driver);
         WaitForElement.init(driver,waitSeconds);
         OtpFlow.setOtp(prop.getProperty("username"), prop.getProperty("appPassword"), "gmail.com");
