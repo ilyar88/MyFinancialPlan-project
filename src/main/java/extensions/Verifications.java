0077ae3c98d8f5c758a7c45424f28c7e46781831
ilyar88 <irahmilevich@gmail.com>
2025-10-11 20:48:06 +0300

    Add allure report to Verifications class.

--- src/main/java/extensions/Verifications.java
+++ src/main/java/extensions/Verifications.java
@@ -4,21 +4,28 @@ import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.asserts.SoftAssert;
 
+import io.qameta.allure.Step;
+
 public class Verifications {
+	
+	@Step("Verify equals")
     public static void verifyEquals(String actual, String expected) {
         Assert.assertEquals(actual, expected,
                 "Verify equals failed: " + actual + " is not equal to: " + expected);
     }
 
+	@Step("Verify text")
     public static void verifyText(String actual, String expected) {
         Assert.assertTrue(expected != null && expected.contains(actual),
                 "Verify equals failed: " + actual + " is not in: " + expected);
     }
     
+	@Step("Assert failed")
     public static void assertFailed(String message) {
         Assert.fail(message);
     }
 
+	@Step("Verify that the element is displayed")
     public static void isDisplayed(WebElement elem, boolean expected) {
         boolean actual = elem != null && elem.isDisplayed();
 
@@ -28,6 +35,7 @@ public class Verifications {
                 " expected displayed=" + expected + " but was " + actual);
     }
     
+	@Step("Verify text using a soft assert")
     public static void verifyTextSoft(SoftAssert sa, String actual, String expected) {
         sa.assertTrue(expected != null && expected.contains(actual),
                 "Verify string (soft) failed: " + actual + " is not in: " + expected);
