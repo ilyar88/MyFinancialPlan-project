fcd699fc1e1a8ae96bf517ba5ce37a126290eea1
ilyar88 <irahmilevich@gmail.com>
2025-10-11 20:50:27 +0300

    Add allure report to RegistrationFlow class.

--- src/main/java/workflows/RegistrationFlow.java
+++ src/main/java/workflows/RegistrationFlow.java
@@ -1,11 +1,13 @@
 package workflows;
 
 import extensions.*;
+import io.qameta.allure.Step;
 import utilities.ManagePages;
 
 
 public class RegistrationFlow {
 	
+	@Step("User registration flow")
 	public static void userRegister(String email, String password) {
 		UiActions.click(ManagePages.login().createAccountLink());
 		UiActions.enterText(ManagePages.register().emailAddress(), email);
