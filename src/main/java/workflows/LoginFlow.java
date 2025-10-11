da8d29f1a7a1f38b9a41d8ff3b698b9c52c35da6
ilyar88 <irahmilevich@gmail.com>
2025-10-11 20:49:33 +0300

    Add allure report to LoginFlow class.

--- src/main/java/workflows/LoginFlow.java
+++ src/main/java/workflows/LoginFlow.java
@@ -2,9 +2,12 @@ package workflows;
 
 import extensions.UiActions;
 import extensions.Verifications;
+import io.qameta.allure.Step;
 import utilities.ManagePages;
 
 public class LoginFlow {
+	
+	@Step("User login to the website flow")
 	public static void userLogin(String email, String password) {
 		UiActions.enterText(ManagePages.login().emailAddress(),email);
 		UiActions.enterText(ManagePages.login().password(), password);
