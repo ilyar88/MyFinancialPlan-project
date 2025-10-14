4bb611ec40226572c7cbe2f7563b245f2789f4d2
ilyar88 <irahmilevich@gmail.com>
2025-10-15 00:01:04 +0300

    Update Verifications class.

--- src/main/java/extensions/Verifications.java
+++ src/main/java/extensions/Verifications.java
@@ -3,41 +3,45 @@ package extensions;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.asserts.SoftAssert;
-
-import io.qameta.allure.Step;
+import io.qameta.allure.Allure;
+import utilities.ManagePages;
 
 public class Verifications {
-	
-	@Step("Verify equals")
+
     public static void verifyEquals(String actual, String expected) {
-        Assert.assertEquals(actual, expected,
-                "Verify equals failed: " + actual + " is not equal to: " + expected);
+        Allure.step("Verify text: " + actual + " is equal to " + expected, () -> {
+            Assert.assertEquals(actual, expected,
+                    "Verify equals failed: " + actual + " is not equal to: " + expected);
+        });
     }
 
-	@Step("Verify text")
     public static void verifyText(String actual, String expected) {
-        Assert.assertTrue(expected != null && expected.contains(actual),
-                "Verify equals failed: " + actual + " is not in: " + expected);
+        Allure.step("Verify test: " + actual + " is in " + expected, () -> {
+            Assert.assertTrue(expected != null && expected.contains(actual),
+                    "Verify text failed: " + actual + " is not in: " + expected);
+        });
     }
-    
-	@Step("Assert failed")
+
     public static void assertFailed(String message) {
-        Assert.fail(message);
+        Allure.step("Assert failed", () -> {
+            Assert.fail(message);
+        });
     }
 
-	@Step("Verify that the element is displayed")
     public static void isDisplayed(WebElement elem, boolean expected) {
-        boolean actual = elem != null && elem.isDisplayed();
-
-        Assert.assertEquals(actual, expected,
-                "Verify display failed, element: " +
-                (elem != null ? elem.toString() : "<null>") +
-                " expected displayed=" + expected + " but was " + actual);
+        Allure.step("Verify element display: " + ManagePages.locatorName(elem), () -> {
+            boolean actual = elem != null && elem.isDisplayed();
+            Assert.assertEquals(actual, expected,
+                    "Verify display failed, element: " + ManagePages.locatorName(elem) +
+                    " expected displayed = " + expected + " but was " + actual);
+        });
     }
-    
-	@Step("Verify text using a soft assert")
-    public static void verifyTextSoft(SoftAssert sa, String actual, String expected) {
-        sa.assertTrue(expected != null && expected.contains(actual),
-                "Verify string (soft) failed: " + actual + " is not in: " + expected);
+
+    public static void verifyTextSoft(String actual, String expected) {
+    	SoftAssert softAssert = new SoftAssert();
+        Allure.step("Verify soft assert text: " + actual + " is in " + expected, () -> {
+        	softAssert.assertTrue(expected != null && expected.contains(actual),
+                    "Verify soft assert failed: " + actual + " is not in: " + expected);
+        });
     }
 }
