a701647d2a8412c02998eff07e66625e7d3237cb
ilyar88 <irahmilevich@gmail.com>
2025-10-15 00:09:39 +0300

    Update RegistrationFlow class.

--- src/main/java/workflows/RegistrationFlow.java
+++ src/main/java/workflows/RegistrationFlow.java
@@ -1,19 +1,19 @@
 package workflows;
 
 import extensions.*;
-import io.qameta.allure.Step;
 import utilities.ManagePages;
-
+import io.qameta.allure.Allure;
 
 public class RegistrationFlow {
-	
-	@Step("User registration flow")
-	public static void userRegister(String email, String password) {
-		UiActions.click(ManagePages.login().createAccountLink());
-		UiActions.enterText(ManagePages.register().emailAddress(), email);
-		UiActions.enterText(ManagePages.register().password(), password);
-		UiActions.enterText(ManagePages.register().passwordConfirmation(), password);
-		UiActions.click(ManagePages.register().CreateAccount());
-		Verifications.isDisplayed(ManagePages.otp().registerImage(),true);
-	}
+
+    public static void userRegister(String email, String password) {
+        Allure.step("User registration flow", () -> {
+            UiActions.click(ManagePages.login().createAccountLink());
+            UiActions.enterText(ManagePages.register().emailAddress(), email);
+            UiActions.enterText(ManagePages.register().password(), password);
+            UiActions.enterText(ManagePages.register().passwordConfirmation(), password);
+            UiActions.click(ManagePages.register().CreateAccount());
+            Verifications.isDisplayed(ManagePages.otp().registerImage(), true);
+        });
+    }
 }
