0fe1c695a5a8cafcc8369a853ec495b40b4f7534
ilyar88 <irahmilevich@gmail.com>
2025-10-14 23:58:31 +0300

    Update SanityTest class.

--- src/main/java/co/il/testing/myfinancialplan/SanityTest.java
+++ src/main/java/co/il/testing/myfinancialplan/SanityTest.java
@@ -2,13 +2,11 @@ package co.il.testing.myfinancialplan;
 
 import java.io.IOException;
 import org.testng.annotations.Ignore;
-import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import io.qameta.allure.*;
 import workflows.*;
 import base.BasePage;
 
-@Listeners(utilities.Listeners.class)
 public class SanityTest extends BasePage {
 	
 	public SanityTest() throws IOException {
@@ -17,19 +15,30 @@ public class SanityTest extends BasePage {
 
 	@Feature("Registration")
 	@Severity(SeverityLevel.CRITICAL)
-	@Description("User register on the website using a username and password.")
-	@Ignore("Skipping until registration flow is stable")
-	@Test
+	@Description("User register to the website with credentials.")
+	@Ignore("Skip user registration.")
+	@Test(description = "User register")
 	public void userRegisterTest() {
 		RegistrationFlow.userRegister(prop.getProperty("username"),prop.getProperty("password"));		
 	}
 	
 	@Feature("Login")
 	@Severity(SeverityLevel.CRITICAL)
-	@Description("Users login on the website using a username and password.")
-	@Test
+	@Description("User login to the website with credentials.")
+	@Test(priority = 1, description = "User login to website")
 	public void userLoginTest() {
 		LoginFlow.userLogin(prop.getProperty("username"),prop.getProperty("password"));	
 		OtpFlow.typePassword();
+	}
+	
+	@Feature("Profile")
+	@Severity(SeverityLevel.CRITICAL)
+	@Description("Create profile with personal details.")
+	@Ignore("Skip create profile")
+	@Test(priority = 2, description = "Create profile")
+	public void createProfileTest() {
+		String[] details = {"Ilya","Rahmilevich","37","1","0","0","2","1","1"};
+		String[] ages = {"1"};
+		ProfileCreationFlow.createProfile(details,"1",ages);	
 	}
 }
