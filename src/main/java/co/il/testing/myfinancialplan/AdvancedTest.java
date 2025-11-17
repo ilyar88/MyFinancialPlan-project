1fa33933ac962dbec62e4c49df5fc82136acf50e
ilyar88 <irahmilevich@gmail.com>
2025-11-17 11:47:41 +0200

    Add resetPassword method and update the class.

--- src/main/java/co/il/testing/myfinancialplan/AdvancedTest.java
+++ src/main/java/co/il/testing/myfinancialplan/AdvancedTest.java
@@ -1,15 +1,17 @@
 package co.il.testing.myfinancialplan;
 
 import java.io.IOException;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import base.BasePage;
-//import extensions.Verifications;
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
+import utilities.TestData;
 import workflows.LoginFlow;
 import workflows.OtpFlow;
+import workflows.ResetPasswordFlow;
 
 public class AdvancedTest extends BasePage {
 
@@ -17,10 +19,18 @@ public class AdvancedTest extends BasePage {
 		super();
 	}
 	
+	@Severity(SeverityLevel.CRITICAL)
+	@Description("Reset password to user (TDD).")
+	@Test(priority = 1, description = "Reset password",dataProvider = "globalProvider", dataProviderClass = TestData.class)
+	public void resetPassword_TDD(String[] username, String[] password, String[] uri) {
+		ResetPasswordFlow.resetPassword(username, password, uri);
+	}
+	
 	@Feature("Login")
 	@Severity(SeverityLevel.CRITICAL)
 	@Description("User login to the website with credentials (TDD).")
-	@Test(priority = 1, description = "User login to website",dataProvider = "loginData", dataProviderClass = LoginFlow.class)
+	@Ignore("Skip user login.")
+	@Test(priority = 2, description = "User login to website",dataProvider = "globalProvider", dataProviderClass = TestData.class)
 	public void userLoginTest_TDD(String username, String password, String uri) {
 		LoginFlow.userLogin_TDD(username, password,uri);
 	}
@@ -28,8 +38,9 @@ public class AdvancedTest extends BasePage {
 	@Feature("OTP")
 	@Severity(SeverityLevel.CRITICAL)
 	@Description("OTP validation (TDD).")
-	@Test(priority = 2, description = "OTP")
-	public void otpTest_TDD() {
-		OtpFlow.typePassword();
+	@Ignore("Skip OTP validation.")
+	@Test(priority = 3, description = "OTP",dataProvider = "globalProvider", dataProviderClass = TestData.class)
+	public void otpTest_TDD(String password, String uri) {
+		OtpFlow.typePassword(password,uri);
 	}
 }
