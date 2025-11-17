6438338a330393fef8c6b4460fb9a48e69b5074e
ilyar88 <irahmilevich@gmail.com>
2025-11-17 13:16:53 +0200

    Update ManagePages class.

--- src/main/java/utilities/ManagePages.java
+++ src/main/java/utilities/ManagePages.java
@@ -2,29 +2,16 @@ package utilities;
 
 import org.openqa.selenium.WebDriver;
 
-import pageObjects.*;
-
 public class ManagePages {
-	
-	private static WebDriver _driver;
-	
-	public static void init(WebDriver driver) {
-		_driver = driver;
+    private static WebDriver _driver;
+
+    public static void init(WebDriver driver) { _driver = driver; }
+
+    public static <T> T page(Class<T> type) {
+        try {
+            return type.getConstructor(WebDriver.class).newInstance(_driver);
+        } catch (Exception e) {
+            throw new RuntimeException("Can't create " + type.getSimpleName(), e);
+        }
     }
-	
-	public static LoginPage login() {
-	    return new LoginPage(_driver);
-	}
-	
-	public static RegisterPage register() {
-	    return new RegisterPage(_driver);
-	}
-	
-	public static OtpPage otp() {
-	    return new OtpPage(_driver);
-	}
-	
-	public static ProfilePage profile() {
-	    return new ProfilePage(_driver);
-	}
 }
