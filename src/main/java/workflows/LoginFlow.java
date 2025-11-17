d2fb0c66c15a90eb8c0fdb11ee46b4086b2ebe84
ilyar88 <irahmilevich@gmail.com>
2025-11-17 13:21:57 +0200

    Update LoginFlow class.

--- src/main/java/workflows/LoginFlow.java
+++ src/main/java/workflows/LoginFlow.java
@@ -1,40 +1,23 @@
 package workflows;
 
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
-
 import extensions.UiActions;
 import utilities.ManagePages;
 import utilities.WaitForElement;
 import io.qameta.allure.Allure;
+import pageObjects.LoginPage;
 
 public class LoginFlow {
 
     public static void userLogin(String email, String password, String uri) {
-        Allure.step("User login flow", () -> {
-            UiActions.enterText(ManagePages.login().emailAddress(), email);
-            UiActions.enterText(ManagePages.login().password(), password);
-            UiActions.click(ManagePages.login().login());
-            WaitForElement.waitUntilUrlContains(uri);
-        });
-    }
-    
-    @DataProvider(name = "loginData")
-    public Object[][] loginData() {
-        return new Object[][]{
-        	{"irahmilevich$gmail.com", "Welcome!", "/auth/login/"},
-            {System.getenv("USERNAME"), "12345", "/auth/login/"},
-            {"irahmilevich@gmail.c2m", "12345678", "/auth/login/"},
-            {"irahmilevichgmail.com", "Welcome!12", "/auth/login/"},
-            {System.getenv("USERNAME"), "Passwrd!", "/auth/login/"},
-            {"", "", "/auth/login/"},
-            {System.getenv("USERNAME"), System.getenv("PASSWORD"), "/auth/verify"}
-        };
+		Allure.step("User login flow", () -> {
+			UiActions.enterText(ManagePages.page(LoginPage.class).emailAddress(), email);
+	        UiActions.enterText(ManagePages.page(LoginPage.class).password(), password);
+	        UiActions.click(ManagePages.page(LoginPage.class).login());
+	        WaitForElement.waitUntilUrlContains(uri);
+		});
     }
 
-    @Test(dataProvider = "loginData")
     public static void userLogin_TDD(String email, String password, String uri) {
     	userLogin(email, password, uri);
     }
-
 }
