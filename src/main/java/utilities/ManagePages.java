e81079e8edd49daeb6e892c9670db31cb94c0e1d
ilyar88 <irahmilevich@gmail.com>
2025-12-20 17:21:32 +0200

    Update ManagePages class by adding the follwoing methods: openNewTab, switchToTab, getUrl and closeTab.

--- src/main/java/utilities/ManagePages.java
+++ src/main/java/utilities/ManagePages.java
@@ -1,17 +1,48 @@
 package utilities;
 
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 public class ManagePages {
     private static WebDriver _driver;
 
-    public static void init(WebDriver driver) { _driver = driver; }
-
+    public static void init(WebDriver driver) {
+    	_driver = driver;
+    }
+    // Creates and returns a page object instance of the given class using the WebDriver
     public static <T> T page(Class<T> type) {
         try {
             return type.getConstructor(WebDriver.class).newInstance(_driver);
         } catch (Exception e) {
             throw new RuntimeException("Can't create " + type.getSimpleName(), e);
         }
+    }
+    // Open new browser tab
+    public static void openNewTab(WebElement elem) {
+        Actions actions = new Actions(_driver);
+        actions.keyDown(Keys.CONTROL).click(elem).keyUp(Keys.CONTROL).perform();
+    }
+    // Switch to the last browser tab
+    public static void switchToTab() {
+        String lastHandle = null;
+        
+        for (String handle : _driver.getWindowHandles()) {
+            lastHandle = handle;
+        }
+        _driver.switchTo().window(lastHandle);
+    }
+    
+	public static void getUrl(String url) {
+		_driver.get(url);
+	}
+	
+	public static String getUrl() {
+	    return _driver.getCurrentUrl();
+	}
+    
+    public static void closeTab() {
+    	_driver.close();
     }
 }
