879261e13566bbb7cd589b954cbbf56757882295
ilyar88 <irahmilevich@gmail.com>
2025-12-20 17:30:07 +0200

    Update IncomesFlow class.

--- src/main/java/workflows/IncomesFlow.java
+++ src/main/java/workflows/IncomesFlow.java
@@ -1,42 +1,40 @@
 package workflows;
 
-import java.util.concurrent.atomic.AtomicInteger;
 import extensions.UiActions;
 import extensions.Verifications;
 import io.qameta.allure.Allure;
 import pageObjects.IncomesPage;
-import utilities.ManagePages;
+import static utilities.ManagePages.page;
 import utilities.WaitForElement;
 
 public class IncomesFlow {
 
-    // This variable counts the number of iterations in the data provider. When it equals to zero, run the verification
-    public static final AtomicInteger ix = new AtomicInteger(0);
-
-    public static void addIncomes(String names, String amounts) {
+    private static float totalIncome = (float) 0.0;
+    
+    public static void addIncomes(String[] names, String[] amounts) {
         Allure.step("Add incomes flow", () -> {
+    	
+        	for(int i=0; i<names.length; i++)
+        	{
+        		UiActions.click(i == 0 ? page(IncomesPage.class).addIncomeOrExpense() 
+        					           : page(IncomesPage.class).addIncomeIcon());
+	
+	            // Fill in the income name and amount, then click the Add button
+	            UiActions.enterText(page(IncomesPage.class).name(), names[i]);
+	            UiActions.enterText(page(IncomesPage.class).amount(), amounts[i]);
+	            UiActions.click(page(IncomesPage.class).add());
+	            totalIncome += Float.parseFloat(amounts[i]);
+        	}
 
-        	int i = ix.getAndIncrement();
-            switch (i) {
-                case 0: //First click on the add income button
-                    UiActions.click(ManagePages.page(IncomesPage.class).addIncome());
-                    break;
-                default: //After adding the first income, click the circle button
-                    UiActions.click(ManagePages.page(IncomesPage.class).addIncomeIcon());
-                    break;
-            }
-
-            // Fill current row (name, amount) and click on add button
-            UiActions.enterText(ManagePages.page(IncomesPage.class).incomeName(), names);
-            UiActions.enterText(ManagePages.page(IncomesPage.class).amount(), amounts);
-            UiActions.click(ManagePages.page(IncomesPage.class).add());
-
-            if (ix.get() == names.length()) {
-                UiActions.click(ManagePages.page(IncomesPage.class).nextButton());
-                String income = UiActions.getText(ManagePages.page(IncomesPage.class).balanceIncome());
-                Verifications.verifyText(income, "14000");
-                WaitForElement.waitUntilUrlContains("/FinancialPlanMaker/monthly_yearly_expenses");
-            }
+            String income = UiActions.getText(page(IncomesPage.class).netSummary());
+            Verifications.verifyText(String.valueOf(totalIncome), income);
+            UiActions.click(page(IncomesPage.class).nextButton());
+            WaitForElement.waitUntilUrlContains("/FinancialPlanMaker/monthly_yearly_expenses");
         });
     }
+    
+	 public static float getTotalIncome()  {
+		 return totalIncome;
+	 }
+
 }
