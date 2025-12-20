a8f1b4aff31d35038a01dd9d88a18028590f1b1b
ilyar88 <irahmilevich@gmail.com>
2025-12-20 17:55:39 +0200

    Update ResetPasswordFlow class.

--- src/main/java/workflows/ResetPasswordFlow.java
+++ src/main/java/workflows/ResetPasswordFlow.java
@@ -1,37 +1,38 @@
 package workflows;
 
-import java.util.concurrent.atomic.AtomicInteger;
-import base.BasePage;
 import extensions.UiActions;
 import io.qameta.allure.Allure;
 import pageObjects.LoginPage;
 import pageObjects.ResetPasswordPage;
-import utilities.ManagePages;
+import static utilities.ManagePages.page;
+import static utilities.ManagePages.getUrl;
 import utilities.WaitForElement;
 
 public class ResetPasswordFlow {
 	
-	private static final AtomicInteger ix = new AtomicInteger(0);
-
-	public static void resetPassword(String[] username, String[] password, String[] uri) {
+	public static void resetPassword(String username, String[] password, String[] uri) {
 		Allure.step("Reset password flow", () -> {
-			int i = ix.get();
-			if(i == 0)
+			
+			for(int i=0; i<password.length; i++)
 			{
-				UiActions.click(ManagePages.page(LoginPage.class).resetPassword());
-				WaitForElement.waitUntilUrlContains("/reset-password");
-				UiActions.enterText(ManagePages.page(LoginPage.class).emailAddress(), username[i]);
-				UiActions.click(ManagePages.page(LoginPage.class).login());
-				OtpFlow.setRegex("^https?://testing\\.myfinancialplan\\.co\\.il/auth/new-password/[^/?#]+$");
-				String url = OtpFlow.getText();
-				BasePage.getUrl(url);
-		        WaitForElement.waitUntilUrlContains("/auth/new-password/");
-			}
-	        UiActions.enterText(ManagePages.page(ResetPasswordPage.class).newPassword(), password[i]);
-	        UiActions.enterText(ManagePages.page(ResetPasswordPage.class).confirmPassword(), password[i]);
-	        UiActions.click(ManagePages.page(LoginPage.class).login());
-	        WaitForElement.waitUntilUrlContains(uri[i]);
-		});
-		ix.getAndIncrement();
+				if(i == 0)
+				{
+					UiActions.click(page(LoginPage.class).resetPassword());
+					WaitForElement.waitUntilUrlContains("/reset-password");
+					UiActions.enterText(page(LoginPage.class).emailAddress(), username);
+					UiActions.click(page(LoginPage.class).login());
+					//Search for this URL in the mailbox
+					OtpFlow.setRegex("^https?://testing\\.myfinancialplan\\.co\\.il/auth/new-password/[^/?#]+$");
+					String url = OtpFlow.getText(); //Get the URL address
+					getUrl(url);
+			        WaitForElement.waitUntilUrlContains("/auth/new-password/");
+				}
+		        UiActions.enterText(page(ResetPasswordPage.class).newPassword(), password[i]);
+		        UiActions.enterText(page(ResetPasswordPage.class).confirmPassword(), password[i]);
+		        UiActions.click(page(LoginPage.class).login());
+		        WaitForElement.waitUntilUrlContains(uri[i]);
+		   }
+	   });
+
 	}
 }
