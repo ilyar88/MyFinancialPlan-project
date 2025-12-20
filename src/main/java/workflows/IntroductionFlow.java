e1de721dba0d9332ee5d52edb48082f8a7a14664
ilyar88 <irahmilevich@gmail.com>
2025-12-20 17:31:11 +0200

    Update IntroductionFlow class.

--- src/main/java/workflows/IntroductionFlow.java
+++ src/main/java/workflows/IntroductionFlow.java
@@ -3,7 +3,7 @@ package workflows;
 import java.util.List;
 import org.openqa.selenium.WebElement;
 import extensions.UiActions;
-import utilities.ManagePages;
+import static utilities.ManagePages.page;
 import utilities.WaitForElement;
 import io.qameta.allure.Allure;
 import pageObjects.IntroductionPage;
@@ -12,12 +12,15 @@ public class IntroductionFlow {
 	
 	public static void introduction() {
 		Allure.step("Introduction flow", () -> {
-			List<WebElement> elements = ManagePages.page(IntroductionPage.class).expandIcons();
+			
+			List<WebElement> elements = page(IntroductionPage.class).expandIcons();
 			for (int i = 0; i < elements.size(); i++) {
+				WaitForElement.waitFor(elements.get(i), "ELEMENT_CLICKABLE", 10);
                 UiActions.click(elements.get(i));
+				WaitForElement.waitFor(elements.get(i), "ELEMENT_CLICKABLE", 10);
                 UiActions.click(elements.get(i));
             }
-			UiActions.click(ManagePages.page(IntroductionPage.class).letsBegin());
+			UiActions.click(page(IntroductionPage.class).letsBegin());
 			WaitForElement.waitUntilUrlContains("/FinancialPlanMaker/Incomes");
 		});
 	}
