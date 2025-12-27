cfb13e58337032f5a51e8bc5c29d0241fc494cd8
ilyar88 <irahmilevich@gmail.com>
2025-12-28 01:30:27 +0200

    Add PricingPage class.

---
+++ src/main/java/pageObjects/PricingPage.java
@@ -0,0 +1,24 @@
+package pageObjects;
+
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class PricingPage {
+	
+	private  WebDriver _driver;
+	
+	public PricingPage(WebDriver driver) {
+		_driver = driver;
+    }
+	//There are three buttons: 1. for base path, 2. for standard path, 3. for Premium path  
+    public List<WebElement> paths() {
+        return _driver.findElements(By.cssSelector("button[type='button']"));
+    }
+    //There two checkboxes for standard and Premium paths
+    public List<WebElement> monthlyPay() {
+        return _driver.findElements(By.cssSelector("input[type='checkbox']"));
+    }
+
+}
