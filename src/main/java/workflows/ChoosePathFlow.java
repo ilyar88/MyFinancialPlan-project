f992564ab374a328deec5c13ea037bc976f9e393
ilyar88 <irahmilevich@gmail.com>
2025-12-28 01:34:16 +0200

    Add MyPlanFlow class.

---
+++ src/main/java/workflows/ChoosePathFlow.java
@@ -0,0 +1,52 @@
+package workflows;
+
+import static utilities.ManagePages.page;
+import extensions.UiActions;
+import extensions.Verifications;
+import io.qameta.allure.Allure;
+import pageObjects.CreditCheckoutPage;
+import pageObjects.OrderSummaryPage;
+import pageObjects.PricingPage;
+import utilities.WaitForElement;
+
+public class ChoosePathFlow {
+	
+	public static void choosePath(String name,String email, String phone) {
+		
+	    Allure.step("Choose path flow", () -> {
+	    	
+	    	float ActualPrice = 0f;
+	    	//Select Premium path
+	    	UiActions.click(page(PricingPage.class).monthlyPay().get(1));
+	        UiActions.click(page(PricingPage.class).paths().get(2));
+	        WaitForElement.waitUntilUrlContains("/orderSummary");
+	        UiActions.enterText(page(OrderSummaryPage.class).fullname(),name);
+	        UiActions.enterText(page(OrderSummaryPage.class).emailAddress(),email);
+	        UiActions.enterText(page(OrderSummaryPage.class).phone(),phone);
+	        for(int i=0; i<page(OrderSummaryPage.class).prices().size()-1; i++)
+	        	ActualPrice += Float.parseFloat(UiActions.getText(page(OrderSummaryPage.class).prices().get(i)));
+	        
+	        String expectedPrice = UiActions.getText(page(OrderSummaryPage.class).prices().get(2));
+	        Verifications.verifyEquals(String.valueOf(ActualPrice),expectedPrice);
+	        UiActions.click(page(OrderSummaryPage.class).beyondPayment());
+	        WaitForElement.waitUntilUrlContains("/credit-checkout");	        
+	    });
+	}
+	
+	public static void credit_checkout(String credit, String year, String month, String id) {
+		
+		Allure.step("Credit checkout flow", () -> {
+			
+			UiActions.enterText(page(CreditCheckoutPage.class).cardNumber(),credit);
+			UiActions.selectOption(page(CreditCheckoutPage.class).year(), "value", year);
+			UiActions.selectOption(page(CreditCheckoutPage.class).month(), "value",month);
+			UiActions.enterText(page(CreditCheckoutPage.class).id(),id);
+			UiActions.click(page(CreditCheckoutPage.class).pay());
+			OtpFlow.setOtp("", "", "gmail.com");
+			OtpFlow.setRegex("^\s*תודה,\s*התשלום\s*בוצע!\s*$");
+			String actualText = OtpFlow.getEmailText();
+			Verifications.verifyEquals(actualText, "תודה, התשלום בוצע!");
+			WaitForElement.waitUntilUrlContains("/home");
+		});
+	}
+}
---
+++ src/main/java/workflows/MyPlanFlow.java
@@ -0,0 +1,86 @@
+package workflows;
+
+import static utilities.ManagePages.page;
+
+import extensions.UiActions;
+import extensions.Verifications;
+import io.qameta.allure.Allure;
+import pageObjects.HomePage;
+import pageObjects.IncomesPage;
+import pageObjects.MyPlanPage;
+import utilities.ManagePages;
+import utilities.WaitForElement;
+
+public class MyPlanFlow {
+	
+	public static void median(String category) {
+		
+		Allure.step("Median flow", () -> {
+
+	        UiActions.click(page(HomePage.class).cards("/myPlan"));
+	        UiActions.click(page(MyPlanPage.class).category());
+	        UiActions.click(page(MyPlanPage.class).chooseCategory(category));
+	        //Extract comparison information about the median salary
+	        var headers = page(MyPlanPage.class).headers();
+	        var amounts = page(MyPlanPage.class).amounts();
+	        var medians = page(MyPlanPage.class).median();
+
+	        for (int i = 0; i < headers.size(); i++) {
+
+	            StringBuilder text = new StringBuilder();
+	            text.append("The median for the ").append(headers.get(i)).append(" with the amount is: ")
+	                 .append(amounts.get(i)).append(", ");
+
+	            UiActions.click(medians.get(i));
+	            UiActions.moveToElement(page(MyPlanPage.class).medianSalary(), 5);
+
+	            String salary = UiActions.getText(page(MyPlanPage.class).medianSalary());
+	            text.append(salary);
+
+	            Allure.step(text.toString());
+	        }
+	        ManagePages.goBack();
+	        WaitForElement.waitUntilUrlContains("/home");
+		});
+	}
+	
+	public static void updateIncomesOrExpenses(String category, String[] names, String[] amounts) {
+
+	    Allure.step("Update incomes or expenses flow", () -> {
+
+	        UiActions.click(page(HomePage.class).cards("/myPlan"));
+	        UiActions.click(page(MyPlanPage.class).category());
+	        UiActions.click(page(MyPlanPage.class).chooseCategory(category));
+
+	        for (int i = 0; i < names.length; i++) {
+
+	            var editIcon = page(MyPlanPage.class).editIcon().get(i);
+
+	            UiActions.moveToElement(editIcon, 10);
+	            WaitForElement.waitFor(editIcon, "ELEMENT_CLICKABLE", 10);
+	            UiActions.click(editIcon);
+
+	            UiActions.enterText(page(IncomesPage.class).name(), names[i]);
+	            UiActions.enterText(page(IncomesPage.class).amount(), amounts[i]);
+	            UiActions.click(page(IncomesPage.class).add());
+
+	            //Click the cancel button if the name is empty
+	            if (UiActions.getText(page(IncomesPage.class).name()).isBlank()) {
+	                UiActions.click(page(IncomesPage.class).cancel());
+	            }
+	            else if (!UiActions.getText(page(IncomesPage.class).amount()).matches("\\d+(\\.\\d+)?")) 
+	                Verifications.assertFailed("Invalid input: amount is not a number.");
+	            else {
+					try { //If an error message appears due to an invalid number, click the cancel button 
+						WaitForElement.waitFor(page(MyPlanPage.class).errorMessage(), "ELEMENT_DISPLAYED", 5);
+					} catch (Exception e) {
+						UiActions.click(page(IncomesPage.class).cancel());
+					}
+				}
+	        }
+	        ManagePages.goBack();
+	        WaitForElement.waitUntilUrlContains("/home");
+	    });
+	}
+
+}
