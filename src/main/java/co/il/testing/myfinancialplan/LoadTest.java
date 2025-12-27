4156b4d65d7a433a247ce06f8e2fcf85f66fb2d7
ilyar88 <irahmilevich@gmail.com>
2025-12-28 00:41:14 +0200

    Update LoadTest class.

--- src/main/java/co/il/testing/myfinancialplan/LoadTest.java
+++ src/main/java/co/il/testing/myfinancialplan/LoadTest.java
@@ -7,14 +7,14 @@ import io.qameta.allure.Description;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
 
-public class LoadTesting {
+public class LoadTest {
 
-	public LoadTesting() throws IOException {
+	public LoadTest() throws IOException {
 		super();
 	}
 	
 	@Severity(SeverityLevel.MINOR)
-	@Description("Run a load test with 10 virtual users and verify that the request returns a 200 status code.")
+	@Description("Execute a load test with 10 virtual users and verify that the requests return a 200 status code.")
 	@Test(description = "Load test with 10 virtual users")
 	void loadTest() throws Exception {
 		LoadTestFlow.loadTest();
