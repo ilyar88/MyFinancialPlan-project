5bc7f209eb4a0c9df7e631490c7ca64a301b29ff
ilyar88 <irahmilevich@gmail.com>
2026-01-03 02:18:28 +0200

    Update ProfileFlow class.

--- src/main/java/workflows/ProfileFlow.java
+++ src/main/java/workflows/ProfileFlow.java
@@ -4,17 +4,23 @@ import extensions.UiActions;
 import io.qameta.allure.Allure;
 import pageObjects.ProfilePage;
 import static utilities.ManagePages.page;
+import java.util.function.BiConsumer;
+import org.openqa.selenium.WebElement;
 import utilities.WaitForElement;
 
-public class ProfileCreationFlow {
+public class ProfileFlow {
 
-	public static void createProfile(String[] profile, String[] dropdowns, String[] kids) {
+	public static void profile(String[] profile, String[] dropdowns, String[] kids) {
 		
-        Allure.step("Create profile flow", () -> {
-        	     	
-            UiActions.enterText(page(ProfilePage.class).firstName(), profile[0]);
-            UiActions.enterText(page(ProfilePage.class).lastName(), profile[1]);
-            UiActions.enterText(page(ProfilePage.class).age(), profile[2]);
+        Allure.step("Create or update profile flow", () -> {
+        	//Enters text into the element if the value is not marked as "Unchanged"     
+        	BiConsumer<WebElement, String> value =
+        		    (el, val) -> { if (!val.contains("Unchanged")) UiActions.enterText(el, val); };
+
+        	value.accept(page(ProfilePage.class).firstName(), profile[0]);
+        	value.accept(page(ProfilePage.class).lastName(), profile[1]);
+        	value.accept(page(ProfilePage.class).age(), profile[2]);
+
             /** 
              * 1) Gender
              * 2) Sole breadwinner
@@ -23,15 +29,22 @@ public class ProfileCreationFlow {
              * 5) City type
              * 6) Residence type */
             for (int i = 0; i < dropdowns.length; i++) {
-                UiActions.selectOption(page(ProfilePage.class).dropdowns().get(i),"value",dropdowns[i]);
+            	if (!dropdowns[i].contains("Unchanged"))
+            		UiActions.selectOption(page(ProfilePage.class).dropdowns().get(i),"value",dropdowns[i]);
             }
             //Number of kids in the family
             WaitForElement.waitFor(page(ProfilePage.class).kidsCount(),"ELEMENT_CLICKABLE", 10);
             UiActions.click(page(ProfilePage.class).kidsCount());
-            UiActions.enterText(page(ProfilePage.class).kidsCount(), kids[0]);
-            for(int i=1; i < kids.length; i++)
-            	UiActions.enterText(page(ProfilePage.class).ages(i), kids[i]);
             
+            for(int i=0; i < kids.length; i++) {
+            	if (kids[i].contains("Unchanged"))
+            		continue;
+            	
+                if (i == 0)
+                	UiActions.enterText(page(ProfilePage.class).kidsCount(), kids[i]);               
+                else
+            		UiActions.enterText(page(ProfilePage.class).ages(i), kids[i]);
+            }            
             WaitForElement.waitFor(page(ProfilePage.class).createProfile(),"ELEMENT_CLICKABLE", 10);
             UiActions.click(page(ProfilePage.class).createProfile());
             WaitForElement.waitUntilUrlContains("/introduction");
